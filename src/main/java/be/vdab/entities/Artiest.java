2338af1f3c619808b88cc6ed8da9d36af74a1ce0
Wouter Boghaert <Wouter Boghaert@192.168.0.104>
2017-12-15 18:14:39 +0100

    Eerste commit, instellingen, entities en valueobject geschreven

---
+++ src/main/java/be/vdab/entities/Artiest.java
@@ -0,0 +1,63 @@
+package be.vdab.entities;
+
+import java.io.Serializable;
+import java.util.Collections;
+import java.util.Set;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="artiesten")
+public class Artiest implements Serializable {
+	private static final long serialVersionUID = 1L;
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long id;
+	private String naam;
+	@OneToMany(mappedBy="artiest")
+	private Set<Album> albums;
+	
+	public long getId() {
+		return id;
+	}
+	
+	public String getNaam() {
+		return naam;
+	}
+	
+	public Set<Album> getAlbums() {
+		return Collections.unmodifiableSet(albums);
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((naam == null) ? 0 : naam.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (!(obj instanceof Artiest))
+			return false;
+		Artiest other = (Artiest) obj;
+		if (naam == null) {
+			if (other.naam != null)
+				return false;
+		} else if (!naam.equals(other.naam))
+			return false;
+		return true;
+	}
+	
+	
+}
---
+++ src/main/java/be/vdab/valueobjects/Track.java
@@ -0,0 +1,48 @@
+package be.vdab.valueobjects;
+
+import java.io.Serializable;
+import java.math.BigDecimal;
+
+import javax.persistence.Embeddable;
+
+@Embeddable
+public class Track implements Serializable {
+	private static final long serialVersionUID = 1L;
+	private String naam;
+	private BigDecimal tijd;
+	
+	public String getNaam() {
+		return naam;
+	}
+	
+	public BigDecimal getTijd() {
+		return tijd;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((naam == null) ? 0 : naam.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (!(obj instanceof Track))
+			return false;
+		Track other = (Track) obj;
+		if (naam == null) {
+			if (other.naam != null)
+				return false;
+		} else if (!naam.equals(other.naam))
+			return false;
+		return true;
+	}
+	
+	
+}
